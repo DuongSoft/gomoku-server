4c026237ded020f9b152eb18e7909a56cea5013f
EL CAPITAN <tranhoangduong1994@gmail.com>
2016-07-25 02:25:01 +0700

    test server

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
 	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
+	socket.on('client-say-hello', function (data) {
 		console.log(data);
+		socket.emit('server-say-hello', 'hello ' + data);
 	});
 });
 
