7500d9faa166aec92d699486961c0c8fa73a58be
EL CAPITAN <tranhoangduong1994@gmail.com>
2016-07-25 02:05:58 +0700

    initialize commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+	// res.sendfile(__dirname + '/index.html');
+	res.json({
+		result: 'ok'
+	});
+});
+
+io.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
+
